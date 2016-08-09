55d6eb1974a60636458082852a0414f58695d6fb
kefala <martin.g.lobo@gmail.com>
2016-08-09 00:26:30 -0300

    Add cat

--- public/javascripts/cat-two/index.js
+++ public/javascripts/cat-two/index.js
@@ -3,19 +3,15 @@ define(['cats'], function(cats) {
         init: function() {
             console.log("Cat", cats);
             cats.forEach(function(cat) {
-              cat.nodeCat = (cat.nodeCat) ? cat.nodeCat : document.getElementById(cat.nodeCatId);
-              cat.node = (cat.node) ? cat.node : document.getElementById(cat.nodeId);
-              cat.node.innerText = cat.counterClicks.toString();
-              cat.nodeCat.innerText += " " + cat.name;
-              cat.nodeCat.addEventListener('click', function() {
-                  cat.counterClicks++;
-                  cat.node.innerText = cat.counterClicks.toString();
-              }, false);
+                cat.nodeCat = (cat.nodeCat) ? cat.nodeCat : document.getElementById(cat.nodeCatId);
+                cat.node = (cat.node) ? cat.node : document.getElementById(cat.nodeId);
+                cat.node.innerText = cat.counterClicks.toString();
+                cat.nodeCat.innerText += " " + cat.name;
+                cat.nodeCat.addEventListener('click', function() {
+                    cat.counterClicks++;
+                    cat.node.innerText = cat.counterClicks.toString();
+                }, false);
             });
-
-            /*
-
-            */
         }
     }
 });
