28928bdd72535e6f9ae4646e5c445cd82ce01323
kefala <martin.g.lobo@gmail.com>
2016-08-08 23:38:52 -0300

    Init

---
+++ public/javascripts/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Load");
+    var elem = document.getElementById('my-elem');
+    elem.addEventListener('click', function() {
+    }, false);
+});
